2893a814dcf2dbda207ef87b7dbf9f065fc776d2
Zen Nitiruj <zennitiruj@gmail.com>
2024-09-26 16:18:10 +0700

    test with composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'zenepay/https-local-docker',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '05b151b84bf5fc2c701aed0ca5a748887bc771f7',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'zenepay/https-local-docker' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '05b151b84bf5fc2c701aed0ca5a748887bc771f7',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
